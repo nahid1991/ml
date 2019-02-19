bca5344c08643059cdb91af93535e1615024346a
Nahid Shaiket <nahidshaiket10300@gmail.com>
2019-02-19 18:43:47 +0600

    started the kaggle problem solution

--- k-nearest-algo/k_nearest_algo_sklearn.py
+++ k-nearest-algo/k_nearest_algo_sklearn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn import preprocessing, model_selection, neighbors
+from sklearn import model_selection, neighbors
 import pandas as pd
 
 df = pd.read_csv('breast-cancer-wisconsin.data.csv')
---
+++ kaggle/house-price-prediction/predict_house_price.py
@@ -0,0 +1,5 @@
+import numpy as np
+from sklearn import model_selection, neighbors
+import pandas as pd
+
+df = pd.read_csv('train.csv')
