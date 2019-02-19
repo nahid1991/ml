d93ed5db5713da7cef5bc93401d8d4827e358aee
Nahid Shaiket <nahidshaiket10300@gmail.com>
2019-02-19 15:29:19 +0600

    r squared with random dataset

--- regressions/google_stock_price_prediction.py
+++ regressions/google_stock_price_prediction.py
@@ -18,14 +18,14 @@ df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df))) - 5
+forecast_out = int(math.ceil(0.1*len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
 X = np.array(df.drop(['label'], 1))
 X = preprocessing.scale(X)
-X = X[:-forecast_out]
 X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
 
 df.dropna(inplace=True)
 y = np.array(df['label'])[:len(X)]
